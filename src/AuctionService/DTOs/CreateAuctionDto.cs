b0742ebf537f44e564a91e69bff17f286a6c8387
Kalyan Thiyagarajan <KThiyagarajan@HudsonInsGroup.com>
2023-08-16 13:10:40 -0400

    Section 2 completed.

---
+++ src/AuctionService/DTOs/CreateAuctionDto.cs
@@ -0,0 +1,28 @@
+﻿namespace AuctionService.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+public class CreateAuctionDto
+{
+
+
+    [Required]  
+    public String  Make { get; set; }
+
+    [Required]  
+    public string Model { get; set; }
+    [Required]  
+    public int Year { get; set;}
+    [Required]  
+    public string Color { get; set; }
+    [Required]  
+    public int Mileage { get; set; }
+
+    [Required]  
+    public string ImageUrl { get; set; }
+    [Required]  
+    public int ReservePrice {get; set;}
+
+    [Required]  
+    public DateTime AuctionEnd {get; set;}
+
+}
